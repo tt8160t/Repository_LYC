eabfd0b48b932ae29d5573d3a166e0ab85e0a48c
tt8160t <tt8160t@yahoo.com.tw>
2023-07-01 19:34:41 +0800

    Assignment

---
+++ Main.kt
@@ -0,0 +1,28 @@
+open class Human(val name: String, val mana: Int = 0 ) {
+    open fun attack() {
+            println("$name use Fist Attack!")
+    }
+}
+
+class Mage (name: String, mana: Int = 0 ) : Human(name, mana) {
+    override fun attack() {
+        println("$name use Fireball!")
+    }
+}
+
+fun whethermana(name: String, mana: Int) :Human {
+    return if (mana == 0) {
+        Human(name, mana)
+    } else{
+        Mage(name, mana)
+    }
+}
+
+fun main() {
+    val human = Human("Tony")
+    human.attack()
+    val mage = Mage("Sally" )
+    mage.attack()
+    val bob = whethermana("Bob", 5)
+    bob.attack()
+}
